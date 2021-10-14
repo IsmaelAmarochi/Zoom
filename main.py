545530bbabc2554cfc290077201f0e039b618dc2
IsmaelAmarochi <61270555+IsmaelAmarochi@users.noreply.github.com>
2021-10-14 16:54:22 +0200

    cm

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-detector = HandDetector(detectionCon=0.8)
+detector = HandDetector(detectionCon=0.7)
 startDist = None
 scale = 0
-cx, cy = 500, 500
-
+cx, cy = 500,500
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
     img1 = cv2.imread("cvarduino.jpg")
 
     if len(hands) == 2:
-        # print("Zoom")
-        detector.fingersUp(hands[0]), detector.fingersUp(hands[1])
-        if detector.fingersUp(hands[0])==[1, 1, 0, 0, 0] and \
-            detector.fingersUp(hands[1])==[1, 1, 0, 0, 0]:
-            # print("Zoom")
+        # print(detector.fingersUp(hands&amp;#91;0]), detector.fingersUp(hands&amp;#91;1]))
+        if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and \
+                detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
+            # print("Zoom Gesture")
             lmList1 = hands[0]["lmList"]
             lmList2 = hands[1]["lmList"]
-
             # point 8 is the tip of the index finger
             if startDist is None:
-                lenght, info, img = detector.findDistance(lmList1[8], lmList2[8], img) # -- For the tips of the fingertips
-                # lenght, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img) -- For the center of the hands
-                # print(lenght)
-                startDist = lenght
+                #length, info, img = detector.findDistance(lmList1&amp;#91;8], lmList2&amp;#91;8], img)
+                length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
 
-            lenght, info, img = detector.findDistance(lmList1[8], lmList2[8], img) # -- For the tips of the fingertips
-            # lenght, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img) -- For the center of the hands
-            scale = int((lenght - startDist) // 2) 
+                startDist = length
+
+            #length, info, img = detector.findDistance(lmList1&amp;#91;8], lmList2&amp;#91;8], img)
+            length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+
+            scale = int((length - startDist) // 2)
             cx, cy = info[4:]
             print(scale)
     else:
@@ -41,9 +39,10 @@ while True:
 
     try:
         h1, w1, _= img1.shape
-        newH, newW = ((h1 + scale)//2)*2, ((w1 + scale)//2)*2
+        newH, newW = ((h1+scale)//2)*2, ((w1+scale)//2)*2
         img1 = cv2.resize(img1, (newW,newH))
-        img[cy-newH//2:cy+newH//2, cx-newW//2:cx+newW//2] = img1
+
+        img[cy-newH//2:cy+ newH//2, cx-newW//2:cx+ newW//2] = img1
     except:
         pass
 
